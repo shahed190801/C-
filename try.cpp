172c6c3e93637ea262e7f3d11c58c22a6b8e62ac
Md. Shahed Hasan <mdshahedhasanshaikh8@gmail.com>
2024-12-26 15:42:42 +0600

    Add files via upload

---
+++ try.cpp
@@ -0,0 +1,231 @@
+#include <iostream>
+using namespace std;
+
+void  classDetails( )
+{
+	string department, batchOrSession, monthOrWeek,  day;
+	
+	cout<<"Input Department: ";
+	cin>> department;
+	
+	cout<<"Input Batch/session: ";
+	cin>> batchOrSession;
+	
+	cout<<"Input Month/week: ";
+	cin>> monthOrWeek;
+	
+	cout<<"Input Day: ";
+	cin>> day;
+	
+	cout<<"Department: "<< department<<endl;
+	cout<<"Batch/session: "<< batchOrSession<<endl;
+	cout<<"Month/week: "<< monthOrWeek<<endl;
+	cout<<"Day: "<< day<<endl;
+}
+
+string st1, st2, st3, st4, st5, st6, st7,st8, st9, st10, st11, st12, st13, st14,st15, st16,st17, st18, st19, st20, st21, st22, st23, st24, st25,
+		  st26, st27, st28, st29, st30, st31, st32, st33, st34, st35, st36, st37, st38, st39, st40, stre1, stre2, stre3, stre4, stre5 ;
+
+void inputStudentId()
+{
+	
+		string studentId[] ={st1, st2, st3, st4, st5, st6, st7,st8, st9, st10, st11, st12, st13, st14,st15, st16,st17, st18, st19, st20, st21, st22, st23, st24, st25,
+		  st26, st27, st28, st29, st30, st31, st32, st33, st34, st35, st36, st37, st38, st39, st40, stre1, stre2, stre3, stre4, stre5};
+		  
+		  
+		cout<< "Input ID1: ";
+		cin>> studentId[0];
+		
+		cout<< "Input ID2: ";
+		cin>> studentId[1];
+		
+		cout<< "Input ID3: ";
+		cin>> studentId[2];
+		
+		cout<< "Input ID4: ";
+		cin>> studentId[3];
+		
+		cout<< "Input ID5: ";
+		cin>> studentId[4];
+		
+		cout<< "Input ID6: ";
+		cin>> studentId[5];
+		
+		cout<< "Input ID7: ";
+		cin>> studentId[6];
+		
+		cout<< "Input ID8: ";
+		cin>> studentId[7];
+		
+		cout<< "Input ID9: ";
+		cin>> studentId[8];
+		
+		cout<< "Input ID10: ";
+		cin>> studentId[9];
+		
+		cout<< "Input ID11: ";
+		cin>> studentId[10];
+		
+		cout<< "Input ID12: ";
+		cin>> studentId[11];
+		
+		cout<< "Input ID13: ";
+		cin>> studentId[12];
+		
+		cout<< "Input ID14: ";
+		cin>> studentId[13];
+		
+		cout<< "Input ID15: ";
+		cin>> studentId[14];
+		
+		cout<< "Input ID16: ";
+		cin>> studentId[15];
+		
+		cout<< "Input ID17: ";
+		cin>> studentId[16];
+		
+		cout<< "Input ID18: ";
+		cin>> studentId[17];
+		
+		cout<< "Input ID19: ";
+		cin>> studentId[18];
+		
+		cout<< "Input ID20: ";
+		cin>> studentId[19];
+		
+		cout<< "Input ID21: ";
+		cin>> studentId[20];
+		
+		cout<< "Input ID22: ";
+		cin>> studentId[21];
+		
+		cout<< "Input ID23: ";
+		cin>> studentId[22];
+		
+		cout<< "Input ID24: ";
+		cin>> studentId[23];
+		
+		cout<< "Input ID25: ";
+		cin>> studentId[24];
+		
+		cout<< "Input ID26: ";
+		cin>> studentId[25];
+		
+		cout<< "Input ID27: ";
+		cin>> studentId[26];
+		
+		cout<< "Input ID28: ";
+		cin>> studentId[27];
+		
+		cout<< "Input ID29: ";
+		cin>> studentId[28];
+		
+		cout<< "Input ID30: ";
+		cin>> studentId[29];
+		
+		cout<< "Input ID31: ";
+		cin>> studentId[30];
+		
+		cout<< "Input ID32: ";
+		cin>> studentId[31];
+		
+		cout<< "Input ID33: ";
+		cin>> studentId[32];
+		
+		cout<< "Input ID1: ";
+		cin>> studentId[33];
+		
+		cout<< "Input ID1: ";
+		cin>> studentId[34];
+		
+		cout<< "Input ID1: ";
+		cin>> studentId[35];
+		
+		cout<< "Input ID1: ";
+		cin>> studentId[36];
+		
+		cout<< "Input ID1: ";
+		cin>> studentId[37];
+		
+		cout<< "Input ID1: ";
+		cin>> studentId[38];
+		
+		cout<< "Input ID1: ";
+		cin>> studentId[39];
+		
+		cout<< "Input ID1: ";
+		cin>> studentId[40];
+		
+		cout<< "Input ID(re1) m: ";
+		cin>> studentId[41];
+		
+		cout<< "Input ID(re2) : ";
+		cin>> studentId[42];
+		
+		cout<< "Input ID(re3) : ";
+		cin>> studentId[43];
+		
+		cout<< "Input ID(re4) : ";
+		cin>> studentId[44];
+		
+		
+
+		cout << "ID1: "<< studentId[0] << endl;
+		cout << "ID2: "<< studentId[1] << endl;
+		cout << "ID3: "<< studentId[2] << endl;
+		cout << "ID4: "<< studentId[3] << endl;
+		cout << "ID5: "<< studentId[4] << endl;
+		cout << "ID6: "<< studentId[5] << endl;
+		cout << "ID7: "<< studentId[6] << endl;
+		cout << "ID8: "<< studentId[7] << endl;
+		cout << "ID9: "<< studentId[8] << endl;
+		cout << "ID10: "<< studentId[9] << endl;
+		cout << "ID11: "<< studentId[10] << endl;
+		cout << "ID12: "<< studentId[11] << endl;
+		cout << "ID13: "<< studentId[12] << endl;
+		cout << "ID14: "<< studentId[13] << endl;
+		cout << "ID15: "<< studentId[14] << endl;
+		cout << "ID16: "<< studentId[15] << endl;
+		cout << "ID17: "<< studentId[16] << endl;
+		cout << "ID18: "<< studentId[17] << endl;
+		cout << "ID19: "<< studentId[18] << endl;
+		cout << "ID20: "<< studentId[19] << endl;
+		cout << "ID21: "<< studentId[20] << endl;
+		cout << "ID22: "<< studentId[21] << endl;
+		cout << "ID23: "<< studentId[22] << endl;
+		cout << "ID24: "<< studentId[23] << endl;
+		cout << "ID25: "<< studentId[24] << endl;
+		cout << "ID26: "<< studentId[25] << endl;
+		cout << "ID27: "<< studentId[26] << endl;
+		cout << "ID28: "<< studentId[27] << endl;
+		cout << "ID29: "<< studentId[28] << endl;
+		cout << "ID30: "<< studentId[29] << endl;
+		cout << "ID31: "<< studentId[30] << endl;
+		cout << "ID32: "<< studentId[31] << endl;
+		cout << "ID33: "<< studentId[32] << endl;
+		cout << "ID34: "<< studentId[33] << endl;
+		cout << "ID35: "<< studentId[34] << endl;
+		cout << "ID36: "<< studentId[35] << endl;
+		cout << "ID37: "<< studentId[36] << endl;
+		cout << "ID38: "<< studentId[37] << endl;
+		cout << "ID39: "<< studentId[38] << endl;
+		cout << "ID40: "<< studentId[39] << endl;
+		cout << "ID(re1) : "<< studentId[40] << endl;
+		cout << "ID40(re2) : "<< studentId[41] << endl;
+		cout << "ID40(re3) : "<< studentId[42] << endl;
+		cout << "ID40(re4) : "<< studentId[43] << endl;
+		cout << "ID40(re5) : "<< studentId[44] << endl;
+		
+}
+
+
+
+int main()
+{
+	cout << "~~~CLASS ATTENDENCE~~~"<< endl;
+	
+	classDetails();
+	inputStudentId();
+	
+   return 0;
+}
