43c1c369e7c77f1ac5eeb700d0ccef83955b34b1
Md. Shahed Hasan <mdshahedhasanshaikh8@gmail.com>
2025-02-04 12:56:47 +0600

    Update leap year using while .cpp

--- leap year using while .cpp
+++ leap year using while .cpp
@@ -6,11 +6,15 @@ int main(){
     cout<< "Enter a year(Enter 0 to stop): ";
         cin>>year;
 
-    while(year >0){
+    while(year!=0){
 
-    if((year% 400 ==0)||(year%4==0 && year% 100!=0)){
+     if(year<0){
+        cout<<"Invalid input!"<<endl;
+     }
+   else if((year% 400 ==0)||(year%4==0 && year% 100!=0)){
         cout<<year<<" is a leap year."<<endl;
     }
+
     else{
         cout<<year<<" isn't a leap year."<<endl;
         }
@@ -18,6 +22,7 @@ int main(){
         cin>>year;
     }
     cout<< ""<<endl;
-    cout<< "Thanks for using This Programme.";
-      cout<< ""<<endl;
+    cout<< "Terminated!"<<endl;
+    cout<< "Thanks for using This Programme."<<endl;
+    cout<< ""<<endl;
 }
